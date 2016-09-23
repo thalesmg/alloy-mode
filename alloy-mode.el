5de0cf10a10a94a5d51f70a4eb7e496cde1bd152
Drew Malzahn <dmalzahn@stevens.edu>
2016-09-23 14:00:47 -0400

    Add full ELPA-compliant header
    See this page: https://www.emacswiki.org/emacs/MakingPackages

--- alloy-mode.el
+++ alloy-mode.el
@@ -16,7 +16,11 @@
 ; along with this program; if not, write to the Free Software
 ; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-; Package-Requires: ()
+;; Author: Allison L Waingold
+;; URL: https://github.com/dwwmmn/alloy-mode
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24"))
+;; Keywords: alloy modeling
 
 (defvar alloy-font-lock-keywords
   ;; keywords
